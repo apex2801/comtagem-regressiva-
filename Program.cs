f70b7d8241379a41946dde089473c5b6a6f1bdbf
apex2801 <aapinheiro2005@gmail.com>
2021-03-07 17:11:23 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace comtagem_regressiva_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+            Console.ForegroundColor = ConsoleColor .Blue;
+            Console.WriteLine("arthur albuquerque pinheiro");
+            Console.WriteLine("primeiro modulo");
+            Console.WriteLine("turma:1l3");
+            Console.ForegroundColor = ConsoleColor .Red;
+            Console.WriteLine("--------------------");
+            Console.WriteLine("contagem regressiva");
+            Console.ForegroundColor = ConsoleColor .Green;
+            Console.WriteLine("--------------------");
+            Console.WriteLine("one");
+            Console.WriteLine("two");
+            Console.WriteLine("three");
+            Console.WriteLine("fuor");
+            Console.WriteLine("five");
+            Console.WriteLine("six");
+            Console.WriteLine("seven");
+            Console.WriteLine("eight");
+            Console.WriteLine("nine");
+            Console.WriteLine("ten");
+            Console.ResetColor();
+            Console.Beep();
+        }
+    }
+}
